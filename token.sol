e6ba59f498e0fb319fbd18017294f4d0bc525903
praveen345qe <151440478+praveen345qe@users.noreply.github.com>
2023-12-02 10:42:47 +0530

    Create token.sol

---
+++ token.sol
@@ -0,0 +1,36 @@
+## Praveen 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+
+    constructor() ERC20("Gaming Token", "GT") Ownable(msg.sender) {
+        uint8 tokenDecimals = 0;
+        uint256 initialTokenSupply = 300; // 300 tokens with 0 decimals
+
+        _mint(_msgSender(), initialTokenSupply * (10**tokenDecimals));
+    }
+
+    function mintTokens(address collector, uint256 TKS) public onlyOwner {
+        require(TKS > 0);
+        require(msg.sender == owner());
+        _mint(collector, TKS);
+    }
+
+    function burnTokens(uint256 TKS) public {
+        require(TKS > 0 );
+        require(TKS <= balanceOf(_msgSender()));
+        _burn(_msgSender(), TKS);
+    }
+
+    function transferTokens(address to, uint256 TKS) public returns (bool) {
+        require(to != address(0));
+        require(TKS > 0);
+        require(TKS <= balanceOf(_msgSender()));
+        _transfer(_msgSender(), to, TKS);
+        return true;
+    }
+}
